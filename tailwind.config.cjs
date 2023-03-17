fa3d42698d021a3d399f0452fc378e517a3459a3
Vincent Chuck <vincent.chuck7@gmail.com>
2023-03-17 21:38:15 +0800

    Run prettier format on not gitignored files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
