8558ba1bd23e408d5d8a11c858cbf91e9d9ccc4f
Vincent Chuck <vincent.chuck7@gmail.com>
2023-03-19 22:37:30 +0800

    Add logo bar for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import { type NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import SearchIcon from '@material-ui/icons/Search';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 import { api } from '~/utils/api';
+import Image from 'next/image';
+import navLogo from 'public/amazon-logo.png';
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: 'from tRPC' });
@@ -25,39 +25,61 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="">
-        <div className="header">
-          <img
-            className="header__logo"
-            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-          />
+        <div className="flex h-12 w-full flex-row flex-nowrap items-center justify-between bg-[#232f3e]">
+          <div className="flex w-auto items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="m-2 block h-8 w-8 text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
 
-          <div className="header__search">
-            <input className="header__searchInput" type="text" />
-            <SearchIcon className="header__searchIcon" />
+            <Image
+              alt="amazon-logo"
+              className="float-left mt-1.5 w-20"
+              src={navLogo}
+            />
           </div>
 
-          <div className="header__nav">
-            <div className="header__option">
-              <span className="header__optionLineOne">Hello Guest</span>
-              <span className="header__optionLineTwo">Sign In</span>
-            </div>
+          <div className="flex w-auto items-center">
+            <div className="text-white">Sign in ›</div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="block h-5 w-5 text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
 
-            <div className="header__option">
-              <span className="header__optionLineOne">Returns</span>
-              <span className="header__optionLineTwo">& Orders</span>
-            </div>
-
-            <div className="header__option">
-              <span className="header__optionLineOne">Your</span>
-              <span className="header__optionLineTwo">Prime</span>
-            </div>
-
-            <div className="header__optionBasket">
-              <ShoppingBasketIcon />
-              <span className="header__optionLineTwo header__basketCount">
-                0
-              </span>
-            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="m-2 block h-8 w-8 text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
           </div>
         </div>
       </main>
