5cca2b0c77746c8cfbe831bd6dcfc9c11b82fb74
Vincent Chuck <vincent.chuck7@gmail.com>
2023-03-29 17:59:17 +0800

    Change breakpoints from sm to md to target larger screens

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -84,26 +84,26 @@ function Layout({ children }: Props) {
       </Head>
       <main className={`${emberFont.variable} font-ember`}>
         {/* navbar container */}
-        <section className="bg-slate-800 text-white sm:bg-slate-900">
+        <section className="bg-slate-800 text-white md:bg-slate-900">
           {/* logo bar */}
-          <section className="flex h-12 w-full flex-row flex-nowrap items-center justify-between sm:h-16 sm:px-2">
+          <section className="flex h-12 w-full flex-row flex-nowrap items-center justify-between md:h-16 md:px-2">
             {/* logo bar left */}
             <div className="flex w-auto flex-shrink-0 items-center">
               <Icon
                 name="hamburger"
                 strokeWidth={1.5}
-                className="m-2 h-8 w-8 sm:hidden"
+                className="m-2 h-8 w-8 md:hidden"
               />
               <Image
                 alt="amazon-logo"
                 priority
-                className="mt-1.5 ml-2 w-20 sm:w-24"
+                className="mt-1.5 ml-2 w-20 md:w-24"
                 src={navLogo}
               />
             </div>
 
             {/* logo bar centre for larger screen */}
-            <div className="mx-4 hidden h-full w-full items-center gap-4 py-3 sm:flex">
+            <div className="mx-4 hidden h-full w-full items-center gap-4 py-3 md:flex">
               {/* deliver to */}
               <div className="flex items-end">
                 <Icon name="location" strokeWidth={1.5} className="h-6 w-6" />
@@ -114,7 +114,7 @@ function Layout({ children }: Props) {
               </div>
 
               {/* search bar for larger screen */}
-              <div className="ml-2 flex h-full w-full items-center justify-between rounded-md bg-white pl-2">
+              <div className="ml-2 flex h-full w-full items-center justify-between whitespace-nowrap rounded-md bg-white pl-2">
                 <div className="flex h-full flex-grow items-center text-lg text-gray-900">
                   Search Amazon
                 </div>
@@ -139,13 +139,13 @@ function Layout({ children }: Props) {
             {/* logo bar right */}
             <div className="ml-2 flex w-auto flex-shrink-0 items-center whitespace-nowrap">
               {/* mobile login button */}
-              <div className="flex items-center sm:hidden">
+              <div className="flex items-center md:hidden">
                 <div>Sign in ›</div>
                 <Icon name="user" strokeWidth={2.5} className="h-6 w-6" />
               </div>
 
               {/* larger screen login button */}
-              <div className="hidden flex-col items-start sm:flex ">
+              <div className="hidden flex-col items-start md:flex ">
                 <span className="text-xs">Hello, sign in</span>
                 <div className="flex items-center">
                   <span className="text-sm font-bold">Account & Orders</span>
@@ -158,7 +158,7 @@ function Layout({ children }: Props) {
               </div>
               <div className="ml-4 flex w-auto items-center justify-center">
                 <Image alt="amazon-cart" className="w-10" src={cartIcon} />
-                <span className="text-md absolute -top-0 font-bold text-orange-300 sm:top-2">
+                <span className="text-md absolute -top-0 font-bold text-orange-300 md:top-2">
                   99
                 </span>
               </div>
@@ -169,7 +169,7 @@ function Layout({ children }: Props) {
           </section>
 
           {/* search bar */}
-          <section className="h-12 px-2.5 pt-[2px] pb-[5px] sm:hidden">
+          <section className="h-12 px-2.5 pt-[2px] pb-[5px] md:hidden">
             <div className="flex h-full w-full items-center justify-between rounded-l-lg rounded-r-xl bg-white pl-2">
               <div className="flex h-full flex-grow items-center text-gray-900">
                 Search Amazon
@@ -185,8 +185,8 @@ function Layout({ children }: Props) {
           </section>
 
           {/* nav links bar */}
-          <nav className="no-scrollbar flex h-11  items-center overflow-x-auto whitespace-nowrap pr-5 sm:h-9 sm:bg-slate-800 sm:text-sm">
-            <div className="hidden items-center font-bold sm:flex">
+          <nav className="no-scrollbar flex h-11  items-center overflow-x-auto whitespace-nowrap pr-5 md:h-9 md:bg-slate-800 md:text-sm">
+            <div className="hidden items-center font-bold md:flex">
               <Icon
                 name="hamburger"
                 strokeWidth={1.5}
@@ -202,7 +202,7 @@ function Layout({ children }: Props) {
           </nav>
 
           {/* nav location bar */}
-          <section className="flex h-10 items-center bg-[#37475A] pl-5 sm:hidden">
+          <section className="flex h-10 items-center bg-[#37475A] pl-5 md:hidden">
             <Icon name="location" strokeWidth={1.5} className="h-5 w-5" />
             <span className="ml-1 text-sm">Deliver to Seattle 98121</span>
           </section>
@@ -219,15 +219,15 @@ function Layout({ children }: Props) {
             <Icon
               name="chevron_up"
               strokeWidth={2.5}
-              className="h-4 w-4 sm:hidden"
+              className="h-4 w-4 md:hidden"
             />
-            <span className="text-xs sm:hidden">TOP OF PAGE</span>
-            <span className="hidden text-sm sm:flex">Back to top</span>
+            <span className="text-xs md:hidden">TOP OF PAGE</span>
+            <span className="hidden text-sm md:flex">Back to top</span>
           </a>
 
           {/* bottom links */}
-          <div className="flex items-center justify-center bg-slate-800 p-4 sm:border-b sm:border-gray-600">
-            <nav className="grid grid-flow-col grid-rows-2 gap-x-24 sm:flex sm:items-center sm:justify-center sm:gap-12">
+          <div className="flex items-center justify-center bg-slate-800 p-4 md:border-b md:border-gray-600">
+            <nav className="grid grid-flow-col grid-rows-2 gap-x-24 md:flex md:items-center md:justify-center md:gap-12">
               <a className="my-1">Your orders</a>
               <a className="my-1">Your account</a>
               <a className="my-1">Browsing History</a>
@@ -236,11 +236,11 @@ function Layout({ children }: Props) {
           </div>
 
           {/* bottom region and language selection */}
-          <div className="flex h-44 flex-col items-center justify-around bg-[#0F1111] p-6 text-gray-300 sm:h-20 sm:flex-row sm:justify-center sm:gap-20 sm:bg-slate-800">
+          <div className="flex h-44 flex-col items-center justify-around bg-[#0F1111] p-6 text-gray-300 md:h-20 md:flex-row md:justify-center md:gap-20 md:bg-slate-800">
             <Image
               alt="amazon-logo"
               priority
-              className="mt-1.5 hidden w-20 sm:flex"
+              className="mt-1.5 hidden w-20 md:flex"
               src={navLogo}
             />
             <div className="flex gap-8">
@@ -254,17 +254,17 @@ function Layout({ children }: Props) {
               </div>
               <span>🇺🇸 United States</span>
             </div>
-            <div className="sm:hidden">
+            <div className="md:hidden">
               <span>Already a customer? </span>
               <a className="font-bold text-white">Sign in</a>
             </div>
-            <div className="text-xs sm:hidden">
+            <div className="text-xs md:hidden">
               © 1996-2023, Amazon.com, Inc. or its affiliates
             </div>
           </div>
 
           {/* bottom copyright notice */}
-          <div className="hidden h-12 items-center justify-center bg-gray-900 text-xs sm:flex">
+          <div className="hidden h-12 items-center justify-center bg-gray-900 text-xs md:flex">
             © 1996-2023, Amazon.com, Inc. or its affiliates
           </div>
         </section>
