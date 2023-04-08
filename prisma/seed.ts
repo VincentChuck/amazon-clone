26098e227489e8730a997020e8ac5e955181d0b6
Vincent Chuck <vincent.chuck7@gmail.com>
2023-04-08 17:34:35 +0800

    Update prisma schema
    - use implicit many-to-many for:
        - product items and product variation options
        - categories product categories and variations
    - allow for non-unique variations and variation options

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,58 @@
-import { PrismaClient, type ProductCategory } from '@prisma/client';
+import { PrismaClient, type ProductItem } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
 const prisma = new PrismaClient();
 async function main() {
-  const books = await prisma.productCategory.create({
-    data: {
-      categoryName: 'Books',
-      childCategory: {
-        create: [
-          {
-            categoryName: 'Self-Help',
-          },
-          {
-            categoryName: 'History',
-          },
-        ],
-      },
+  const booksCatName = ['Self-Help', 'History'];
+  const booksCategory = {
+    categoryName: 'Books',
+    childCategory: {
+      create: [
+        ...[...booksCatName].map((name) => {
+          return {
+            categoryName: name,
+            variations: {
+              create: {
+                variationName: 'Formats',
+                variationOptions: {
+                  create: [
+                    { value: 'Kindle' },
+                    { value: 'Audiobook' },
+                    { value: 'Hardcover' },
+                    { value: 'Paperback' },
+                  ],
+                },
+              },
+            },
+          };
+        }),
+      ],
     },
+  };
+
+  const books = await prisma.productCategory.upsert({
+    where: { categoryName: 'Books' },
+    update: {},
+    create: booksCategory,
   });
+
+  // function randomBook(): ProductItem {
+  //   const name = `The ${faker.word.noun()} of ${faker.word.noun()}`;
+  //   const description = faker.lorem.paragraphs(2);
+  //   const category = Math.floor(Math.random() * booksCatName.length);
+  //   return {
+  //     name,
+  //     description,
+  //     category,
+  //     productItems: {
+  //       create: {},
+  //     },
+  //   };
+  // }
+
   console.log({ books });
 }
+
 main()
   .then(async () => {
     await prisma.$disconnect();
