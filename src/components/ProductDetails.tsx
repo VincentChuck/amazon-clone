3f304ec940a758ccccc702d4c869c494bedcf25f
Vincent Chuck <vincent.chuck7@gmail.com>
2023-06-12 16:49:47 +0800

    Format price and layout on product details page

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -5,6 +5,7 @@ import type {
   VariationArr,
   productResponse,
 } from '~/types';
+import Icon from './Icon';
 
 type Props = {
   product: productResponse;
@@ -29,7 +30,7 @@ export default function ProductDetails({
 
   return (
     <div className="grid h-full w-full grid-cols-1 p-3 md:max-w-[1500px] md:grid-cols-3 md:py-8 lg:grid-cols-[384px_auto_244px]">
-      <h1 className="my-2 text-sm md:hidden">{product.name}</h1>
+      <h1 className="my-2 text-sm text-[#565959] md:hidden">{product.name}</h1>
       <div className="relative h-96 w-full bg-gray-100 md:aspect-square md:h-auto">
         <Image
           alt={`${product.name} product image`}
@@ -41,16 +42,25 @@ export default function ProductDetails({
       </div>
 
       <div className="hidden flex-grow px-2 md:block">
-        <h1 className="my-2 text-lg font-bold">{product.name}</h1>
+        <h1 className="my-2 text-2xl">{product.name}</h1>
         <div className="flex items-center">
           <div className="m-2 flex-grow p-1">
-            <div>{active.price.toString()}</div>
-            <span className="font-bold">About this item</span>
-            <div>{product.description}</div>
+            <div className="flex align-top text-sm">
+              <span>$</span>
+              <span className="relative -top-1 text-3xl">
+                {active.price.toString().split('.')[0]}
+              </span>
+              <span>{active.price.toString().split('.')[1]}</span>
+            </div>
             {variationArr.map((variation) => {
               return (
                 <div key={variation.variation} className="my-2">
-                  <span>{variation.variation}: </span>
+                  <span className="text-sm">
+                    {variation.variation}:{' '}
+                    <span className="font-bold">
+                      {active.variationOption.value}
+                    </span>
+                  </span>
                   <div className="flex gap-2">
                     {variation.options.map((option) => {
                       const isActive = active.variationOptionId === option.id;
@@ -70,17 +80,36 @@ export default function ProductDetails({
                 </div>
               );
             })}
+            <span className="font-bold">About this item</span>
+            <div>{product.description}</div>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col px-4 py-8 outline-1 outline-gray-300 md:outline">
-        <div>{active.price.toString()}</div>
+      <div className="flex h-fit flex-col px-4 py-8 outline-1 outline-gray-300 md:outline">
+        <div className="flex align-top text-sm">
+          <span>$</span>
+          <span className="relative -top-1 text-3xl">
+            {active.price.toString().split('.')[0]}
+          </span>
+          <span>{active.price.toString().split('.')[1]}</span>
+        </div>
+        <div className="my-1 flex items-center">
+          <Icon
+            name="location"
+            strokeWidth={1.5}
+            className="mr-2 inline h-5 w-5"
+          />
+          <span className="text-[#007185] md:text-xs">
+            Deliver to Seattle 98121
+          </span>
+        </div>
+        <div className="text-lg text-[#007600]">In Stock</div>
         <div className="items-center">
-          <button className="my-2 w-full rounded-3xl py-2 text-lg outline md:py-1 md:text-base">
+          <button className="my-2 w-full rounded-3xl bg-[#FFD814] py-2 text-lg outline outline-1 outline-[#FCD200] md:py-1 md:text-base">
             Add to cart
           </button>
-          <button className="my-2 w-full rounded-3xl py-2 text-lg outline md:py-1 md:text-base">
+          <button className="my-2 w-full rounded-3xl bg-[#FFA41C] py-2 text-lg outline outline-1 outline-[#FF8F00] md:py-1 md:text-base">
             Buy now
           </button>
         </div>
